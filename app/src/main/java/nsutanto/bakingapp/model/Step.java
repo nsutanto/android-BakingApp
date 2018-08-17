80bb3524e508a751726a2c8ce933a91c4e39748c
Nicholas Sutanto <nsutanto@enova.com>
2018-08-17 00:50:36 -0500

    get it compile

---
+++ app/src/main/java/nsutanto/bakingapp/model/Step.java
@@ -0,0 +1,31 @@
+package nsutanto.bakingapp.model;
+
+import java.io.Serializable;
+
+public class Step implements Serializable {
+    private int id;
+    private String shortDescription;
+    private String description;
+    private String videoURL;
+    private String thumbnailURL;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVideoURL() {
+        return videoURL;
+    }
+
+    public String getThumbnailURL() {
+        return thumbnailURL;
+    }
+}
