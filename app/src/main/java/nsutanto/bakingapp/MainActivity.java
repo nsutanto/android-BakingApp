6cf49dc4b9607c1e13cb0358e879f2f78cfcf45a
Nicholas Sutanto <nsutanto@enova.com>
2018-08-17 13:15:30 -0500

    feat: Implement Listener

--- app/src/main/java/nsutanto/bakingapp/MainActivity.java
+++ app/src/main/java/nsutanto/bakingapp/MainActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import nsutanto.bakingapp.fragment.RecipeFragment;
+import nsutanto.bakingapp.listener.IRecipeFragmentListener;
+import nsutanto.bakingapp.model.Recipe;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IRecipeFragmentListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +26,10 @@ public class MainActivity extends AppCompatActivity {
         fm.beginTransaction()
                 .add(R.id.recipe_container, recipeFragment)
                 .commit();
+    }
+
+    @Override
+    public void OnRecipeClick(Recipe recipe) {
+
     }
 }
