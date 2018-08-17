a69c203d23f28b4b34391896f13a68fc914d3004
Nicholas Sutanto <nsutanto@enova.com>
2018-08-17 00:50:09 -0500

    get it compile

--- app/src/main/java/nsutanto/bakingapp/MainActivity.java
+++ app/src/main/java/nsutanto/bakingapp/MainActivity.java
@@ -1,7 +1,10 @@
 package nsutanto.bakingapp;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import nsutanto.bakingapp.fragment.RecipeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupFragment();
+    }
+
+    private void setupFragment() {
+        FragmentManager fm = getFragmentManager();
+
+        RecipeFragment recipeFragment = new RecipeFragment();
+
+        fm.beginTransaction()
+                .add(R.id.recipe_container, recipeFragment)
+                .commit();
     }
 }
