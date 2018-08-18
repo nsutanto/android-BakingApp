5c25491146164a1b0c0fbe773f3c1c22768ec71f
Nicholas Sutanto <nsutanto@enova.com>
2018-08-18 08:25:01 -0500

    feat: Add step detail fragment

---
+++ app/src/main/java/nsutanto/bakingapp/fragment/StepDetailFragment.java
@@ -0,0 +1,50 @@
+package nsutanto.bakingapp.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import nsutanto.bakingapp.R;
+
+public class StepDetailFragment extends Fragment {
+
+    @BindView(R.id.tv_description)
+    TextView tv_description;
+
+    @BindView(R.id.tv_short_description)
+    TextView tv_short_description;
+
+    String shortDescription = "";
+    String description = "";
+
+
+    public StepDetailFragment() {
+    }
+
+    public void setStepDetail(String shortDescription, String description) {
+
+        this.shortDescription = shortDescription;
+        this.description = description;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_step_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        setupUI();
+        return view;
+    }
+
+    private void setupUI() {
+        tv_description.setText(description);
+        tv_short_description.setText(shortDescription);
+    }
+
+
+}
