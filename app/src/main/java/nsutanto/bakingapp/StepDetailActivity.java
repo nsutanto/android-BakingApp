993813a17246b1e21d34d097f3c49f649ac2b4c7
Nicholas Sutanto <nsutanto@enova.com>
2018-08-19 18:08:00 -0500

    Implement on configuration changes

--- app/src/main/java/nsutanto/bakingapp/StepDetailActivity.java
+++ app/src/main/java/nsutanto/bakingapp/StepDetailActivity.java
@@ -2,8 +2,10 @@ package nsutanto.bakingapp;
 
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -17,6 +19,9 @@ import nsutanto.bakingapp.model.Step;
 public class StepDetailActivity extends AppCompatActivity {
 
     private Step step;
+    FragmentManager fm = getFragmentManager();
+    StepDetailFragment stepDetailFragment = new StepDetailFragment();
+    MediaFragment mediaFragment = new MediaFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,19 +42,32 @@ public class StepDetailActivity extends AppCompatActivity {
     private void setupFragment() {
         FragmentManager fm = getFragmentManager();
 
-        StepDetailFragment stepDetailFragment = new StepDetailFragment();
         stepDetailFragment.setStepDetail(step.getShortDescription(), step.getDescription());
-
         fm.beginTransaction()
                 .add(R.id.step_detail_container, stepDetailFragment)
                 .commit();
 
         if (!step.getVideoURL().equals("") || !step.getThumbnailURL().equals("")) {
-            MediaFragment mediaFragment = new MediaFragment();
             mediaFragment.setMediaURL(step.getVideoURL(), step.getThumbnailURL());
 
             fm.beginTransaction()
                     .add(R.id.media_container, mediaFragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            fm.beginTransaction()
+                    .remove(stepDetailFragment)
+                    .commit();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            fm.beginTransaction()
+                    .add(R.id.step_detail_container, stepDetailFragment)
                     .commit();
         }
     }
