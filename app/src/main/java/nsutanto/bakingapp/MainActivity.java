0ba2b54b2f35eeae92fe2d2f166631079526c0c2
Nicholas Sutanto <nsutanto@enova.com>
2018-08-20 22:45:48 -0500

    feat: Setup widget

--- app/src/main/java/nsutanto/bakingapp/MainActivity.java
+++ app/src/main/java/nsutanto/bakingapp/MainActivity.java
@@ -1,21 +1,28 @@
 package nsutanto.bakingapp;
 
 import android.app.FragmentManager;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RemoteViews;
 
 import nsutanto.bakingapp.fragment.RecipeFragment;
 import nsutanto.bakingapp.listener.IRecipeFragmentListener;
+import nsutanto.bakingapp.model.Ingredient;
 import nsutanto.bakingapp.model.Recipe;
 
 public class MainActivity extends AppCompatActivity implements IRecipeFragmentListener {
 
+    private int widgetID;
+    boolean isWidget = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initWidget();
         setupFragment();
     }
 
@@ -31,10 +38,44 @@ public class MainActivity extends AppCompatActivity implements IRecipeFragmentLi
 
     @Override
     public void OnRecipeClick(Recipe recipe) {
-        Intent intent = new Intent(this, StepActivity.class);
-        Bundle bundle = new Bundle();
-        bundle.putSerializable("recipe", recipe);
-        intent.putExtras(bundle);
-        startActivity(intent);
+        if (isWidget) {
+            setupWidget(recipe);
+        } else {
+            Intent intent = new Intent(this, StepActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("recipe", recipe);
+            intent.putExtras(bundle);
+            startActivity(intent);
+        }
+    }
+
+    private void initWidget() {
+        if (getIntent().getExtras() != null) {
+            if(getIntent().getExtras().containsKey(AppWidgetManager.EXTRA_APPWIDGET_ID)) {
+                isWidget = true;
+                widgetID = getIntent().getExtras().getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+            }
+        }
+    }
+
+    private void setupWidget(Recipe recipe)
+    {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+
+        RemoteViews views = new RemoteViews(this.getPackageName(), R.layout.baking_app_widget);
+        views.setTextViewText(R.id.tv_recipe, recipe.getName());
+
+        String listIngredientStr = "";
+        for (Ingredient ingredient : recipe.getIngredients())
+        {
+            listIngredientStr += ingredient.getIngredient() + "\n";
+        }
+        views.setTextViewText(R.id.tv_ingredients, listIngredientStr);
+        appWidgetManager.updateAppWidget(widgetID, views);
+
+        Intent intent = new Intent();
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
