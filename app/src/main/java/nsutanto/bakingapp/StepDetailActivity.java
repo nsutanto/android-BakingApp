09971e6ff8fe0c33a4ed89a7405a2d6acc3067a6
Nicholas Sutanto <nsutanto@enova.com>
2018-08-20 19:59:57 -0500

    feat: Add on click navigation

--- app/src/main/java/nsutanto/bakingapp/StepDetailActivity.java
+++ app/src/main/java/nsutanto/bakingapp/StepDetailActivity.java
@@ -1,27 +1,29 @@
 package nsutanto.bakingapp;
 
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
-
 import java.util.List;
 
-import nsutanto.bakingapp.fragment.IngredientFragment;
 import nsutanto.bakingapp.fragment.MediaFragment;
+import nsutanto.bakingapp.fragment.NavigationFragment;
 import nsutanto.bakingapp.fragment.StepDetailFragment;
-import nsutanto.bakingapp.model.Ingredient;
-import nsutanto.bakingapp.model.Recipe;
+import nsutanto.bakingapp.listener.INavigationFragmentListener;
 import nsutanto.bakingapp.model.Step;
 
-public class StepDetailActivity extends AppCompatActivity {
+public class StepDetailActivity extends AppCompatActivity implements INavigationFragmentListener{
 
     private Step step;
-    FragmentManager fm = getFragmentManager();
-    StepDetailFragment stepDetailFragment = new StepDetailFragment();
-    MediaFragment mediaFragment = new MediaFragment();
+    private List<Step> steps;
+    private int currentStepID;
+    private Boolean isMediaAdded = false;
+    private FragmentManager fm = getFragmentManager();
+    private StepDetailFragment stepDetailFragment = new StepDetailFragment();
+    private MediaFragment mediaFragment = new MediaFragment();
+    private NavigationFragment navigationFragment = new NavigationFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,8 @@ public class StepDetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         step = (Step) bundle.getSerializable("step");
+        currentStepID = step.getId();
+        steps = (List<Step>) bundle.getSerializable("steps");
     }
 
     private void setupFragment() {
@@ -53,7 +57,15 @@ public class StepDetailActivity extends AppCompatActivity {
             fm.beginTransaction()
                     .add(R.id.media_container, mediaFragment)
                     .commit();
+
+            isMediaAdded = true;
         }
+
+        navigationFragment.setStep(currentStepID, steps.size());
+        fm.beginTransaction()
+                .add(R.id.navigation_container, navigationFragment)
+                .commit();
+
     }
 
     @Override
@@ -65,14 +77,60 @@ public class StepDetailActivity extends AppCompatActivity {
             fm.beginTransaction()
                     .remove(stepDetailFragment)
                     .commit();
+            fm.beginTransaction()
+                    .remove(navigationFragment)
+                    .commit();
         } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
             fm.beginTransaction()
                     .add(R.id.step_detail_container, stepDetailFragment)
+                    .commit();
+            fm.beginTransaction()
+                    .add(R.id.navigation_container, navigationFragment)
                     .commit();
         }
     }
 
     private void setupUI() {
         setTitle(step.getShortDescription());
+    }
+
+    public void OnPreviousBtnClick() {
+        currentStepID--;
+
+        updateFragments(currentStepID);
+    }
+    public void OnNextBtnClick() {
+        currentStepID++;
+        updateFragments(currentStepID);
+    }
+
+    private void updateFragments(int currentStepID) {
+        step = steps.get(currentStepID);
+
+        FragmentTransaction transaction = fm.beginTransaction();
+
+        if (!step.getVideoURL().equals("") || !step.getThumbnailURL().equals("")) {
+
+            mediaFragment.setMediaURL(step.getVideoURL(), step.getThumbnailURL());
+
+            if (!isMediaAdded) {
+                transaction.add(R.id.media_container, mediaFragment);
+                isMediaAdded = true;
+            } else {
+                mediaFragment.resetMedia();
+            }
+        } else {
+            transaction.remove(mediaFragment);
+            isMediaAdded = false;
+        }
+
+
+
+        stepDetailFragment.setStepDetail(step.getShortDescription(), step.getDescription());
+        stepDetailFragment.resetStepDetail();
+
+        navigationFragment.resetCurrentStepID(currentStepID);
+
+        transaction.commit();
     }
 }
